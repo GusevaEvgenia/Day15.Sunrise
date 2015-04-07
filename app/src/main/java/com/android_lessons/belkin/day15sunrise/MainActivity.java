d919687ccebc877f4d9d6bd6b5e9986cbfaeeab4
Belka <Evgenia_@email.ua>
2015-04-07 14:48:22 +0300

    add animation sun and clock

--- app/src/main/java/com/android_lessons/belkin/day15sunrise/MainActivity.java
+++ app/src/main/java/com/android_lessons/belkin/day15sunrise/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,26 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Получим ссылку на солнце
+        ImageView sun = (ImageView) findViewById(R.id.sun);
+        // Анимация для восхода солнца
+        Animation sunRise = AnimationUtils.loadAnimation(this, R.anim.sun_rise);
+        // Подключаем анимацию к нужному View
+        sun.startAnimation(sunRise);
+
+        // Получим ссылку на часы
+        ImageView clock = (ImageView) findViewById(R.id.clock);
+        // анимация для вращения часов
+        Animation clockTurn = AnimationUtils.loadAnimation(this, R.anim.clock_turn);
+        clock.startAnimation(clockTurn);
+
+        // получим ссылку на часовую стрелку
+        ImageView hour = (ImageView) findViewById(R.id.hour_hand);
+// анимация для стрелки
+        Animation hourTurn = AnimationUtils.loadAnimation(this, R.anim.hour_turn);
+// присоединяем анимацию
+        hour.startAnimation(hourTurn);
     }
 
 
